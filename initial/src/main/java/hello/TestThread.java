530e1f60c183c6d84ae3326d1f41140539f04dcb
daguangPro <ytdaguang_shan@126.com>
2019-02-14 20:20:39 +0800

    N producer  vs  N Consumer

--- initial/src/main/java/hello/TestThread.java
+++ initial/src/main/java/hello/TestThread.java
@@ -18,6 +18,9 @@ public class TestThread {
 
 		new Thread(consumer).start();
 		new Thread(producer).start();
+
+		new Thread(consumer).start();
+		new Thread(producer).start();
 	}
 
 	private static List<String> tasks = new ArrayList<String>();
@@ -32,20 +35,21 @@ public class TestThread {
 
 		@Override
 		public void run() {
-			logger.info("Consumer start....");
+			String nameStr = Thread.currentThread().getName();
+			logger.info("Consumer start...." + nameStr);
 			while (true) {
-				logger.info("consumer checking...");
+				logger.info("consumer"+nameStr+" checking...");
 				synchronized (tasks) {
 					if (tasks.isEmpty()) {
 						try {
-							logger.info("consumer wait...");
+							logger.info("consumer"+nameStr+" wait...");
 							tasks.wait();
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					} else {
-						logger.info("consume #######get:" + tasks.get(0));
+						logger.info("consume"+nameStr+" ----get:" + tasks.get(0));
 						tasks.clear();
 						tasks.notify();
 					}
@@ -65,18 +69,19 @@ public class TestThread {
 
 		@Override
 		public void run() {
-			logger.info("Producer start....");
+			String nameStr = Thread.currentThread().getName();
+			logger.info("Producer start...." + nameStr);
 			while (true) {
 				synchronized (tasks) {
 					if (tasks.isEmpty()) {
 						Random rand = new Random();
 						String bt = Integer.toString(rand.nextInt(20));
-						logger.info("producer =====push notify:" + bt);
+						logger.info("producer" + nameStr + " =====push notify:" + bt);
 						tasks.add(bt);
 						tasks.notify();
 					} else {
 						try {
-							logger.info("producer wait");
+							logger.info("producer" + nameStr + " wait");
 							tasks.wait();
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
