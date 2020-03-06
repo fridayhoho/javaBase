90d8261618b19ca8b58b4a466d9f02bf4175794b
daguangPro <ytdaguang_shan@126.com>
2020-03-06 11:24:33 +0800

    ThreadPool fix  schedule

--- helloInte/src/main/java/com/test/concurrent/TestThreadPool.java
+++ helloInte/src/main/java/com/test/concurrent/TestThreadPool.java
@@ -1,7 +1,9 @@
 package com.test.concurrent;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * ClassName: TestThreadPool
@@ -11,26 +13,70 @@ import java.util.concurrent.ThreadPoolExecutor;
  * version    V1.0
  */
 public class TestThreadPool {
-	public static void main(String[] args) {
+	/**
+	 * 模拟mysql 里有100w条记录
+	 */
+	private static AtomicInteger dataCountsInMysql = new AtomicInteger(100_0000);
+	/**
+	 * IO 密集型任务：由于线程并不是一直在运行，所以可以尽可能的多配置线程，比如 CPU 个数 * 2
+	 * 导数据属于IO密集型
+	 * <p>
+	 * CPU 密集型任务（大量复杂的运算）应当分配较少的线程，比如 CPU 个数相当的大小
+	 */
+	private static final int POOL_SIZE = Runtime.getRuntime().availableProcessors() * 2;
 
+	private static ThreadPoolExecutor threadPoolExecutor;
+
+	private static boolean isRunning = false;
+	private static Random random = new Random();
+	/**
+	 * 定时器  Timer
+	 */
+	private static ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(2);
+	public static void main(String[] args) {
+		scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName()+" cur:"+ System.currentTimeMillis());
+			}
+		}, 0,  1, TimeUnit.SECONDS);
+
+//		threadPoolExecutor =
+//				(ThreadPoolExecutor) Executors.newFixedThreadPool(POOL_SIZE);
+//		isRunning = true;
+//		addOneWorker();
+//		addOneWorker();
+//		addOneWorker();
+//
+//		System.out.println(threadPoolExecutor.getQueue().size());
+//
+//		try {
+//			Thread.sleep(4000);
+//			isRunning = false;
+//			threadPoolExecutor.shutdown();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 	}
 
-	public  static void testPool(){
-		ThreadPoolExecutor executor =
-				(ThreadPoolExecutor) Executors.newFixedThreadPool(2);
-		executor.submit(() -> {
-			Thread.sleep(1000);
-			return null;
+	private static void addOneWorker(){
+		/**
+		 * 匿名内部类
+		 */
+		threadPoolExecutor.submit(new Runnable() {
+			@Override
+			public void run() {
+				Thread.currentThread().setName(" thread"+ random.nextInt(20));
+				while(isRunning) {
+					int dataLeft = dataCountsInMysql.decrementAndGet();
+					System.out.println(Thread.currentThread().getName() + " dataLeft: " + dataLeft);
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		});
-		executor.submit(() -> {
-			Thread.sleep(1000);
-			return null;
-		});
-		executor.submit(() -> {
-			Thread.sleep(1000);
-			return null;
-		});
-
-
 	}
 }
